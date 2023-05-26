b406e957cf517c224a1046723b002ad965381afe
David Jonathan <davidjonathan@outlook.com>
2023-05-26 20:02:40 -0300

    integraçãoFlutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/material.dart';
 
 import 'ui/home_page.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart'; // new
+import 'package:go_router/go_router.dart';               // new
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';                 // new
+
+import 'app_state.dart';                                 // new
+import 'home_page.dart';
+
 
 void main() {
-  runApp(const MyApp());
+   
+  // Modify from here...
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(ChangeNotifierProvider(
+    create: (context) => ApplicationState(),
+    builder: ((context, child) => const App()),
+  ));
+  // ...to here.
 }
 
 class MyApp extends StatelessWidget {
